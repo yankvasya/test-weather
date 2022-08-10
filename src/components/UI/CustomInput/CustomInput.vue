0a88da0174860b57a1b354d6650e2ec997bbc11f
yankvasya <yankvasya@gmail.com>
2022-08-11 02:41:00 +0300

    feat(UI): created UI/customInput component

---
+++ src/components/UI/CustomInput/CustomInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="custom-input" :class="{ active: active }">
+    <input
+      @keydown.enter.prevent="downEnterInput"
+      @keyup.enter.prevent="upEnterInput"
+      class="custom-input__input"
+      type="text"
+      v-model="value"
+    />
+    <a class="custom-input__btn" href="#">
+      <img
+        class="custom-input__img"
+        src="../../../assets/icons/utils/enter.svg"
+        alt="Нажмите Enter"
+      />
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CustomInput",
+  data() {
+    return {
+      value: "",
+      active: false,
+    };
+  },
+  methods: {
+    downEnterInput() {
+      this.active = true;
+    },
+    upEnterInput() {
+      this.active = false;
+      this.$emit("updated:value");
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped src="./index.scss" />
