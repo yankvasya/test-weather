c34df5935915d4a1bab0f656439ea0fc82becd81
yankvasya <yankvasya@gmail.com>
2022-08-11 03:23:02 +0300

    feat(CitySettings): created component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <WeatherList />
+  <WeatherList v-if="false" />
+  <CitySettings />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import WeatherList from "@/components/WeatherList/WeatherList.vue";
+import CitySettings from "@/components/CitySettings/CitySettings.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     WeatherList,
+    CitySettings,
   },
 });
 </script>
---
+++ src/components/CitySettings/CitySettings.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="city-settings">
+    <a class="city-settings__close-settings close-settings" href="#">
+      <img
+        class="close-settings__img"
+        src="../../assets/icons/utils/close.svg"
+        alt="close"
+      />
+    </a>
+
+    <h3 class="city-settings__title">Settings</h3>
+
+    <ul class="city-settings__cities">
+      <li v-for="n of 2" :key="n" class="city-settings__city">
+        <CityItem />
+      </li>
+    </ul>
+
+    <div class="city-settings__add-location add-location">
+      <h4 class="add-location__title">Add location:</h4>
+      <CustomInput />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import CityItem from "@/components/CityItem/CityItem.vue";
+import CustomInput from "@/components/UI/CustomInput/CustomInput.vue";
+
+export default defineComponent({
+  name: "CitySettings",
+  components: {
+    CityItem,
+    CustomInput,
+  },
+});
+</script>
+
+<style lang="scss" src="./index.scss" />
