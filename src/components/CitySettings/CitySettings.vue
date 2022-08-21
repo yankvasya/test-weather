cb85e9bbba83a1a47c81049cda435fc96ef6eceb
yankvasya <yankvasya@gmail.com>
2022-08-21 03:54:24 +0300

    feat(components/CitySettings): added events and typing

--- src/components/CitySettings/CitySettings.vue
+++ src/components/CitySettings/CitySettings.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="city-settings">
-    <a class="city-settings__close-settings close-settings" href="#">
+    <a
+      @click.prevent="openSettings"
+      class="city-settings__close-settings close-settings"
+      href="#"
+    >
       <img
         class="close-settings__img"
         src="../../assets/icons/utils/close.svg"
@@ -11,28 +15,50 @@
     <h3 class="city-settings__title">Settings</h3>
 
     <ul class="city-settings__cities">
-      <li v-for="n of 2" :key="n" class="city-settings__city">
-        <CityItem />
+      <li v-for="city of cities" :key="city.name" class="city-settings__city">
+        <CityItem :name="city.name" @delete:item="deleteCity" />
       </li>
     </ul>
 
+    <div v-if="!cities.length">Нет добавленных городов!</div>
+
     <div class="city-settings__add-location add-location">
       <h4 class="add-location__title">Add location:</h4>
-      <CustomInput />
+      <CustomInput @updated:value="addCity" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, PropType } from "vue";
 import CityItem from "@/components/CityItem/CityItem.vue";
 import CustomInput from "@/components/UI/CustomInput/CustomInput.vue";
+import { ICityWeather } from "@/types/types";
 
 export default defineComponent({
   name: "CitySettings",
+  props: {
+    cities: {
+      type: Array as PropType<ICityWeather[]>,
+    },
+  },
+  emits: ["updated:addCity", "updated:deleteCity", "updated:openSettings"],
   components: {
     CityItem,
     CustomInput,
+  },
+  methods: {
+    addCity(value: string) {
+      if (value.trim()) {
+        this.$emit("updated:addCity", value);
+      }
+    },
+    deleteCity(value: string) {
+      this.$emit("updated:deleteCity", value);
+    },
+    openSettings() {
+      this.$emit("updated:openSettings", false);
+    },
   },
 });
 </script>
