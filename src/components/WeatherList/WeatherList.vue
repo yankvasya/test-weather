36fe034618ece6ecec4f61d79f18d827d5069403
yankvasya <yankvasya@gmail.com>
2022-08-21 03:56:42 +0300

    feat(components/WeatherList): added props using, emits and typing

--- src/components/WeatherList/WeatherList.vue
+++ src/components/WeatherList/WeatherList.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="weather-list">
-    <a class="weather-list__open-settings open-settings" href="#">
+    <a
+      @click.prevent="openSettings"
+      class="weather-list__open-settings open-settings"
+      href="#"
+    >
       <img
         class="open-settings__img"
         src="../../assets/icons/utils/settings.svg"
@@ -8,22 +12,35 @@
       />
     </a>
 
-    <ul class="weather-list__list">
-      <li v-for="n of 2" :key="n" class="weather-list__item">
-        <weather-item />
+    <ul v-if="cities.length" class="weather-list__list">
+      <li v-for="city of cities" :key="city.name" class="weather-list__item">
+        <weather-item :city="city" />
       </li>
     </ul>
+
+    <h3 v-else>Не сохраненных городов</h3>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, PropType } from "vue";
 import WeatherItem from "@/components/WeatherItem/WeatherItem.vue";
+import { ICityWeather } from "@/types/types";
 
 export default defineComponent({
   name: "WeatherList",
+  props: {
+    cities: {
+      type: Object as PropType<ICityWeather[]>,
+    },
+  },
   components: {
     WeatherItem,
+  },
+  methods: {
+    openSettings() {
+      this.$emit("update:openSettings", true);
+    },
   },
 });
 </script>
