3209b9b02b3170c108e62fc8f78cfc8b330bbe3e
yankvasya <yankvasya@gmail.com>
2022-09-12 23:39:59 +0300

    feat: [components/WeatherItem] added get wind direction

--- src/components/WeatherItem.ce.vue
+++ src/components/WeatherItem.ce.vue
@@ -22,9 +22,11 @@
           <img
             class="detail__image"
             src="../assets/icons/weather/sse.svg"
-            alt="SSE"
+            alt="wind direction"
           />
-          <h5 class="detail__text">{{ city.wind.speed }}m/s SSE</h5>
+          <h5 class="detail__text">
+            {{ city.wind.speed }}m/s {{ getWindDirection }}
+          </h5>
         </li>
         <li class="weather__detail detail">
           <img
@@ -65,6 +67,42 @@ export default defineComponent({
         `../assets/images/weathers/${props.city.weather[0].main.toLowerCase()}.png`,
         import.meta.url
       ).href;
+    },
+    getWindDirection(props: { city: ICityWeather }) {
+      const deg = props.city.wind.deg;
+      if (deg >= 22.5 && deg < 45) {
+        return "NNE";
+      } else if (deg >= 45 && deg < 67.5) {
+        return "NE";
+      } else if (deg >= 67.5 && deg < 90) {
+        return "ENE";
+      } else if (deg >= 90 && deg < 112.5) {
+        return "E";
+      } else if (deg >= 112.5 && deg < 135) {
+        return "ESE";
+      } else if (deg >= 135 && deg < 157.5) {
+        return "SE";
+      } else if (deg >= 157.5 && deg < 180) {
+        return "SSE";
+      } else if (deg >= 180 && deg < 202.5) {
+        return "S";
+      } else if (deg >= 202.5 && deg < 225) {
+        return "SSW";
+      } else if (deg >= 225 && deg < 247.5) {
+        return "SW";
+      } else if (deg >= 247.5 && deg < 270) {
+        return "WSW";
+      } else if (deg >= 270 && deg < 292.5) {
+        return "W";
+      } else if (deg >= 292.5 && deg < 315) {
+        return "WNW";
+      } else if (deg >= 315 && deg < 337.5) {
+        return "NW";
+      } else if (deg >= 337.5 && deg < 360) {
+        return "NNW";
+      } else {
+        return "N";
+      }
     },
   },
 });
--- src/types/types.ts
+++ src/types/types.ts
@@ -17,6 +17,7 @@ export enum WeatherValues {
 
 interface IWind {
   speed: number;
+  deg: number;
 }
 
 interface IMain {
